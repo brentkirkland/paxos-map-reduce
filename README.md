0fabea80b90fe0be9a2b2cf016e481c015cc71cd
kirky <brentckirkland@gmail.com>
2017-05-25 17:34:42 -0700

    updated ish

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # PRM
+
+Note: This program runs from a single machine.
+
+## How to run:
+
+First terminal:
+
+`python run.py 1`
+
+Second terminal:
+
+`python run.py 2`
+
+Third terminal:
+
+`python run.py 3`
+
+You then enter commands into each terminal. Sorry if the formatting may be wonky.
+
+## Completed Commands
+
+* map
+* reduce
--- main.py
+++ main.py
@@ -4,79 +4,80 @@ from reducer import Reducer
 from multiprocessing import Process
 import time
 
-def createCLI():
-    cli = CLI();
-    cli.log('Starting Up');
+class Main:
 
-    # this maps the CLI looks pretty
-    time.sleep(1);
-    cli.log('Loading 3...');
-    time.sleep(1);
-    cli.log('Loading 2...');
-    time.sleep(1);
-    cli.log('Loading 1...');
-    time.sleep(1);
+    def __init__(self, num):
+        self.num = int(num)
 
-    # pass details
-    cli.defineMapperOne(5001, "127.0.0.1");
-    cli.defineMapperTwo(5002, "127.0.0.1");
-    cli.defineReducer(5003, "127.0.0.1");
+    def createCLI(self):
+        cli = CLI();
+        cli.log('Starting Up');
 
-    cli.log('Enter commands below. Use ctrl+c to exit.')
-    boo = True
-    while (boo):
-        try:
-            command = raw_input('> ')
-            cli.handleCommand(command.split());
-        except KeyboardInterrupt:
-            boo = False;
+        # this maps the CLI looks pretty
+        time.sleep(1);
+        cli.log('Loading 3...');
+        time.sleep(1);
+        cli.log('Loading 2...');
+        time.sleep(1);
+        cli.log('Loading 1...');
+        time.sleep(1);
 
-    # exiting
-    cli.log('Exiting');
+        # pass details
+        cli.defineMapperOne(5001, "127.0.0.1");
+        cli.defineMapperTwo(5002, "127.0.0.1");
+        cli.defineReducer(5003, "127.0.0.1");
 
-def createMapperOne():
-    mapperOne = Mapper(5001, "127.0.0.1", 1);
-    mapperOne.log('Starting Up');
-    # execution code goes here
+        cli.log('Enter commands below. Use ctrl+c to exit.')
+        boo = True
+        while (boo):
+            try:
+                command = raw_input('> ')
+                cli.handleCommand(command.split());
+            except KeyboardInterrupt:
+                boo = False;
 
-    # mapperOne.map('test/first.txt', 0, 0)
-    mapperOne.listen()
+        # exiting
+        cli.log('Exiting');
 
-    # exiting
-    mapperOne.log('Exiting');
+    def createMapperOne(self):
+        mapperOne = Mapper(5001 + (self.num-1)*10, "127.0.0.1", 1);
+        mapperOne.log('Starting Up');
+        # execution code goes here
 
-def createMapperTwo():
-    mapperTwo = Mapper(5002, "127.0.0.1", 2);
-    mapperTwo.log('Starting Up');
-    # execution code goes here
+        # mapperOne.map('test/first.txt', 0, 0)
+        mapperOne.listen()
 
-    # mapperTwo.map('test/second.txt', 0, 0)
-    mapperTwo.listen()
+        # exiting
+        mapperOne.log('Exiting');
 
-    # exiting
-    mapperTwo.log('Exiting');
+    def createMapperTwo(self):
+        mapperTwo = Mapper(5002 + (self.num-1)*10, "127.0.0.1", 2);
+        mapperTwo.log('Starting Up');
+        # execution code goes here
 
-def createReducer():
-    reducer = Reducer(5003, "127.0.0.1");
-    reducer.log('Starting Up');
-    # execution code goes here
+        # mapperTwo.map('test/second.txt', 0, 0)
+        mapperTwo.listen()
 
-    reducer.listen()
-    # exiting
-    reducer.log('Exiting');
+        # exiting
+        mapperTwo.log('Exiting');
 
+    def createReducer(self):
+        reducer = Reducer(5003 + (self.num-1)*10, "127.0.0.1");
+        reducer.log('Starting Up');
+        # execution code goes here
 
-def createProcesses():
-    p2 = Process(target=createMapperOne);
-    p3 = Process(target=createMapperTwo);
-    p4 = Process(target=createReducer);
-
-    p2.start();
-    p3.start();
-    p4.start();
-
-    createCLI();
+        reducer.listen()
+        # exiting
+        reducer.log('Exiting');
 
 
-if __name__ == "__main__":
-    createProcesses();
+    def createProcesses(self):
+        p2 = Process(target=self.createMapperOne);
+        p3 = Process(target=self.createMapperTwo);
+        p4 = Process(target=self.createReducer);
+
+        p2.start();
+        p3.start();
+        p4.start();
+
+        self.createCLI();
